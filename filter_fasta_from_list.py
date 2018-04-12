e665eeec503fc1231e1b32f2aeaf65df283e375f
Melissa Chanderban <melissa.ny.ok@gmail.com>
2018-04-12 17:24:41 -0500

    added some comments

--- filter_fasta_from_list.py
+++ filter_fasta_from_list.py
@@ -5,24 +5,25 @@ from Bio import SeqIO
 import argparse
 
 # create an ArgumentParser object ('parser') that will hold all the info necessary to parse the command line
-parser = argparse.ArgumentParser(description="This script filters out user-specified sequences from a FASTA file")
+parser = argparse.ArgumentParser(description="This script filters out user-specified sequence(s) from a FASTA file")
 
 # use the add_argument() method to add a positional argument 
 # positional arguments are required inputs, so their order/position matters
 # argparse treats all options as strings unless told to do otherwise
 parser.add_argument("fasta", help="name of FASTA file")
 
-parser.add_argument("remove", help="name of file containing sequences to be removed")
+parser.add_argument("remove", help="name of file containing sequence(s) to be removed")
 
 # parse the arguments
 args = parser.parse_args()
 
 print("We're gonna open this FASTA file:", args.fasta)
 
-print("filter out sequences contained in this file", args.remove)
+print("filter out sequence(s) contained in this file", args.remove)
 
 fasta = SeqIO.parse(args.fasta, "fasta")
 
+# This works if the file has one header. Use parse if more than one
 remove = SeqIO.read(args.remove, "fasta")
 
 for sequence in fasta:
